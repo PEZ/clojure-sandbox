47e6270049a27c0ed40aa9237528e1240024fa57
Peter Strömberg <pez@pezius.com>
2019-12-09 10:24:14 +0100

    Add pirate lang

---
+++ lab/src/pez/lab.clj
@@ -0,0 +1,7 @@
+(ns pez.lab
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ lab/test/pez/lab_test.clj
@@ -0,0 +1,7 @@
+(ns pez.lab-test
+  (:require [clojure.test :refer :all]
+            [pez.lab :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
